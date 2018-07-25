774b88e586641653ebc7ef51b74e86ec69b8240a
py <604131805@qq.com>
2018-07-25 10:42:39 +0800

    1

--- AI/DataSet.py
+++ AI/DataSet.py
@@ -1,11 +1,18 @@
-import csv
 import glob
 import os
+
 import cv2
 import h5py
 import numpy as np
 import tensorflow as tf
 from PIL import Image
+
+
+def create_folder(class_num):
+    for i in range(class_num):
+        if not os.path.exists('train/'+str(i)):
+            os.makedirs('train/'+str(i))
+    print('%d 类文件夹生成成功' % (class_num))
 
 
 def create_mydataset():
@@ -19,13 +26,6 @@ def create_mydataset():
             fw.write(path+'/'+image_files[i]+'/'+ each_image + ' %d\n'% i)
     print('生成my_dataset.txt文件成功\n')
     fw.close()
-
-    
-def create_folder(class_num):
-    for i in range(class_num):
-        if not os.path.exists('train/'+str(i)):
-            os.makedirs('train/'+str(i))
-    print('%d 类文件夹生成成功' % (class_num))
 
     
 def rename_img(path):
@@ -56,197 +56,6 @@ def rename_folder(path):
     print('over')
 
 
-# class DataSet:
-#     def __init__(self):
-#         with h5py.File('./data_set/data.h5', 'r') as f:
-#             x, y = f['x_data'].value, f['y_data'].value
-
-#         self.train_x, self.test_x, self.train_y, self.test_y = \
-#             train_test_split(x, y, test_size=0.2, random_state=0)
-
-#         self.train_size = len(self.train_x)
-
-#     def get_train_batch(self, batch_size=64):
-#         # 随机获取batch_size个训练数据
-#         choice = np.random.randint(self.train_size, size=batch_size)
-#         batch_x = self.train_x[choice, :]
-#         batch_y = self.train_y[choice, :]
-
-#         return batch_x, batch_y
-
-#     def get_test_set(self):
-#         return self.test_x, self.test_y
-
-
-def create_csv():
-    imgarray = []
-    list = os.listdir('train')  # 列出文件夹下所有的目录与文件
-    for i in range(0, len(list)):
-        path = os.path.join('train', list[i])
-        imgs = glob.glob(path+'\*')
-        for img in imgs:   
-            img = Image.open(img).convert('L')
-            img = img.resize((50, 50))
-            img_ndarray = np.asarray(img, dtype='float64') / 255.0   # 归一化
-            img_ndarray = np.ndarray.flatten(img_ndarray)      # 压平为一维向量  一维数组
-            img_ndarray1 = img_ndarray.tolist()                 # 将array格式转为list格式
-            img_ndarray1.extend([i])                    # 末尾添加
-            imgarray.append(img_ndarray1)                # 只有转化为list格式才能使用append方法
-    with open('train.csv', 'w') as myfile:
-        mywriter = csv.writer(myfile)
-        mywriter.writerows(imgarray)          # list格式才可以使用mywriter写入  array和matrix格式都不可以
-        print('csv创建成功')
-
- 
-def create_npy(img_width, img_heigh, class_num):
-    X, Y = [], []
-    with open('my_dataset.txt','r') as f:
-        for line in f:  
-            img_path, img_label = line.strip().split(' ')
-            img = Image.open(img_path).convert('L')
-            img = img.resize((img_width, img_heigh))
-            img = 1 - np.reshape(img, img_width*img_heigh) / 255.0   # 图片像素值映射到0 - 1之间
-            X.append(img)
-            label_one_hot = [0 if i != int(img_label) else 1 for i in range(class_num)]
-            Y.append(label_one_hot)
-    np.save('X.npy', np.array(X))
-    np.save('Y.npy', np.array(Y)) 
-    print('npy创建成功')
-   
-
-def shuffle_npy(x_train, y_train):   #h5py无法使用
-    print('shuffle前:  ' + str(y_train[0]))
-    index=np.arange(len(y_train))
-    np.random.shuffle(index)
-    x_train=x_train[index] #X_train是训练集，y_train是训练标签
-    y_train=y_train[index]
-    print('shuffle后:  ' + str(y_train[0]))
-    return x_train, y_train
-
-    
-
-def create_h5py(img_width, img_heigh, class_num):  
-    h5f = h5py.File('train.h5', 'w')
-    with open('my_dataset.txt', 'r') as f:
-        img_num = len(f.readlines())
-    dataset = h5f.create_dataset("my_datas", (img_num, img_width, img_heigh, 1), dtype='float32')
-    labels = h5f.create_dataset("my_labels", (img_num, class_num), dtype='int32')
-    temp_data = np.empty((img_num, img_width, img_heigh, 1), dtype="float32")
-    temp_label = np.empty((img_num, class_num), dtype="int32")
-    i = 0
-    with open('my_dataset.txt','r') as f:
-        for line in f:  
-            img_path, img_label = line.strip().split(' ')
-            one_hot_labels = [0 if i != int(img_label) else 1 for i in range(class_num)]
-            img = Image.open(img_path).convert('L')
-            img = img.resize((img_width, img_heigh))
-            img = 1-np.array(img) / 255.0  # 图片像素值映射到 0 - 1之间
-            temp_data[i, :, :, :] = np.asarray(img, dtype="float32").reshape(img_width, img_heigh, 1)
-            temp_label[i, :] = one_hot_labels
-            i += 1
-
-    dataset[:, :, :, :] = temp_data
-    labels[:, :] = temp_label
-          
-    print(labels.shape)
-    print(dataset.shape)               
-    h5f.close()
-    print('h5py创建成功')
-
-
-def create_h5py_batch(batchsize, img_width, img_heigh, class_num):
-    h5f = h5py.File('train.h5', 'w')
-    dataset = h5f.create_dataset("my_datas", (batchsize, img_width, img_heigh, 1), maxshape=(None, img_width, img_heigh, 1), dtype='float32')
-    labels = h5f.create_dataset("my_labels", (batchsize, class_num), maxshape=(None, class_num), dtype='int32')
-    temp_data = np.empty((batchsize, img_width, img_heigh, 1), dtype="float32")
-    temp_label = np.empty((batchsize, class_num), dtype="int32")
-    i = 0
-    times = 0
-    with open('my_dataset.txt', 'r') as f:
-        for line in f:  
-            img_path, img_label = line.strip().split(' ')
-            one_hot_labels = [0 if i != int(img_label) else 1 for i in range(class_num)]
-            img = Image.open(img_path).convert('L')  # 灰度化
-            img = img.resize((img_width, img_heigh))
-            img = 1-np.array(img) / 255.0  # 图片像素值映射到 0 - 1之间 float类型
-            temp_data[i, :, :, :] = np.asarray(img, dtype="float32").reshape(img_width, img_heigh, 1)
-            temp_label[i, :] = one_hot_labels
-            # print(i)
-            i += 1   
-            if i == batchsize:
-                dataset.resize([times*batchsize + batchsize, img_width, img_heigh, 1])
-                dataset[times*batchsize:times*batchsize+batchsize, :, :, :] = temp_data
-                labels.resize([times*batchsize + batchsize, class_num])
-                labels[times*batchsize:times*batchsize+batchsize, :] = temp_label
-                # print(labels.shape)
-                # print(dataset.shape)
-                times += 1
-                i = 0        
-    h5f.close()
-    print('h5py创建成功')
-               
-
-def create_TFrecords(path, img_width, img_heigh,  classes_num):
-    num_classes = [i for i in range(classes_num)]
-    cwd = os.getcwd()
-    writer = tf.python_io.TFRecordWriter(path+".tfrecords")
-    for index, name in enumerate(num_classes):
-        class_path = cwd + "\\"+path+"\\" + str(name) + "\\"
-        for img_name in os.listdir(class_path):
-            img = Image.open(class_path + img_name).convert('L')  # 转化为灰度
-            img = img.resize((img_width, img_heigh))
-            img_raw = img.tobytes()  # 将图片转化为原生bytes
-            example = tf.train.Example(features=tf.train.Features(feature={
-                "my_labels": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
-                'my_datas': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
-            }))
-            writer.write(example.SerializeToString())
-    writer.close()
-    print('TFrecord创建成功')
- 
-
-def read_TFrecords(filename):
-    reader = tf.TFRecordReader()
-    filename_queue = tf.train.string_input_producer([filename])
-    _, serialized_example = reader.read(filename_queue)
-    features = tf.parse_single_example(serialized_example,
-                                       features={
-                                           'my_labels': tf.FixedLenFeature([], tf.int64),
-                                           'my_datas': tf.FixedLenFeature([], tf.string), })
-    img = tf.decode_raw(features['my_datas'], tf.uint8)
-    img = tf.reshape(img, [28, 28, 1])
-    img = 1 - tf.cast(img, tf.float32) / 255.0
-    label = tf.cast(features['my_labels'], tf.int32)
-    return img, label
-
-
-def get_batch_TFrecords(imgs, labels, n_classes, batch_size):
-    imgs_batch, labels_batch = tf.train.shuffle_batch([imgs, labels], batch_size=batch_size, capacity=2000, min_after_dequeue=1000)
-    labels_batch = tf.one_hot(labels_batch, n_classes)
-    labels_batch = tf.cast(labels_batch, dtype=tf.int64)
-    labels_batch = tf.reshape(labels_batch, [batch_size, n_classes])
-    return imgs_batch, labels_batch              
-
-
-def read_h5py():
-    h5f = h5py.File('train.h5', 'r')
-    data = h5f['my_datas']
-    label = h5f['my_labels']
-    print(label[15058])
-    print(data.shape)
-    cv2.imshow('ss', data[15058])
-    cv2.waitKey(0)
-    
-
-def read_npy():
-    x, y = np.load('X.npy'), np.load('Y.npy')
-    x = x.reshape(-1, 28, 28, 1)
-    print(x.shape)
-    print(y.shape)
-    return x, y
-
-
-
 def mnist_to_img():
     from tensorflow.examples.tutorials.mnist import input_data
     mnist_data = input_data.read_data_sets('MNIST_data/')
@@ -265,28 +74,190 @@ def mnist_to_img():
     print('over')
 
 
-  
-#rename(r"train\1")
-#create_folder(5)
-     
-##mnist_to_img()
-#create_mydataset()
-##
-##create_npy(28, 28, 10) 
+'''
+csv
+'''
+def create_csv():
+    import csv
+    imgarray = []
+    list = os.listdir('train')  # 列出文件夹下所有的目录与文件
+    for i in range(0, len(list)):
+        path = os.path.join('train', list[i])
+        imgs = glob.glob(path+'\*')
+        for img in imgs:   
+            img = Image.open(img).convert('L')
+            img = img.resize((50, 50))
+            img_ndarray = np.asarray(img, dtype='float64') / 255.0   # 归一化
+            img_ndarray = np.ndarray.flatten(img_ndarray)      # 压平为一维向量  一维数组
+            img_ndarray1 = img_ndarray.tolist()                 # 将array格式转为list格式
+            img_ndarray1.extend([i])                    # 末尾添加
+            imgarray.append(img_ndarray1)                # 只有转化为list格式才能使用append方法
+    with open('train.csv', 'w') as myfile:
+        mywriter = csv.writer(myfile)
+        mywriter.writerows(imgarray)          # list格式才可以使用mywriter写入  array和matrix格式都不可以
+        print('csv创建成功')
 
-#create_TFrecords('train',28, 28, 10)
+
+'''
+npy
+'''
+def create_npy(w, h, class_num):
+    X, Y = [], []
+    with open('my_dataset.txt','r') as f:
+        for line in f:  
+            img_path, img_label = line.strip().split(' ')
+            img = Image.open(img_path).convert('L')
+            img = img.resize((w, h))
+            img = 1 - np.reshape(img, w*h) / 255.0   # 图片像素值映射到0 - 1之间
+            X.append(img)
+            label_one_hot = [0 if i != int(img_label) else 1 for i in range(class_num)]
+            Y.append(label_one_hot)
+    np.save('X.npy', np.array(X))
+    np.save('Y.npy', np.array(Y)) 
+    print('y[0]:  ' + y[0])
+    cv2.imshow('y[0]',y[0] 
+    cv2.waitKey(0)
+    print('npy创建成功')
+   
+
+def read_npy(w, h, channel=1, shuffle=True):
+    x, y = np.load('X.npy'), np.load('Y.npy')
+    x = x.reshape(-1, w, h, channel)
+    print('x.shape:  ' + str(x.shape))
+    print('y.shape:  ' + str(y.shape))
+    if shuffle:
+        print('shuffle前y[0]:  ' + str(y[0]))
+        index=np.arange(len(y))
+        np.random.shuffle(index)
+        x=x[index] #X_train是训练集，y_train是训练标签
+        y=y[index]
+        print('shuffle后y[0]:  ' + str(y[0]))
+    return x, y
 
 
 
+'''
+h5py
+'''
+def create_h5py(w, h, class_num, shuffle=True):  
+    h5f = h5py.File('train.h5', 'w')
+    with open('my_dataset.txt', 'r') as f:
+        img_num = len(f.readlines())
+    x = h5f.create_dataset("my_datas", (img_num, w, h, 1), dtype='float32')
+    y = h5f.create_dataset("my_labels", (img_num, class_num), dtype='int32')
+    temp_x = np.empty((img_num, w, h, 1), dtype="float32")
+    temp_y = np.empty((img_num, class_num), dtype="int32")
+    i = 0
+    with open('my_dataset.txt','r') as f:
+        for line in f:  
+            img_path, img_label = line.strip().split(' ')
+            one_hot_labels = [0 if i != int(img_label) else 1 for i in range(class_num)]
+            img = Image.open(img_path).convert('L')
+            img = img.resize((w, h))
+            img = 1-np.array(img) / 255.0  # 图片像素值映射到 0 - 1之间
+            temp_x[i, :, :, :] = np.asarray(img, dtype="float32").reshape(w, h, 1)
+            temp_y[i, :] = one_hot_labels
+            i += 1
+    if shuffle:
+        print('shuffle前y[0]:  ' + str(temp_y[0]))
+        index=np.arange(len(temp_y))
+        np.random.shuffle(index)
+        temp_x=temp_x[index] #X_train是训练集，y_train是训练标签
+        temp_y=temp_y[index]
+        print('shuffle后y[0]:  ' + str(temp_y[0]))
+    x[:, :, :, :] = temp_x
+    y[:, :] = temp_y
+    print('y.shape:  ' + str(y.shape))
+    print('x.shape:  ' + str(x.shape))    
+    print('y[0]:  ' + y[0])
+    cv2.imshow('y[0]',y[0] 
+    cv2.waitKey(0)       
+    h5f.close()
+    print('h5py创建成功')
+
+
+def create_h5py_batch(batchsize, w, h, class_num):
+    h5f = h5py.File('train.h5', 'w')
+    dataset = h5f.create_dataset("my_datas", (batchsize, w, h, 1), maxshape=(None, w, h, 1), dtype='float32')
+    labels = h5f.create_dataset("my_labels", (batchsize, class_num), maxshape=(None, class_num), dtype='int32')
+    temp_data = np.empty((batchsize, w, h, 1), dtype="float32")
+    temp_label = np.empty((batchsize, class_num), dtype="int32")
+    i = 0
+    times = 0
+    with open('my_dataset.txt', 'r') as f:
+        for line in f:  
+            img_path, img_label = line.strip().split(' ')
+            one_hot_labels = [0 if i != int(img_label) else 1 for i in range(class_num)]
+            img = Image.open(img_path).convert('L')  # 灰度化
+            img = img.resize((w, h))
+            img = 1-np.array(img) / 255.0  # 图片像素值映射到 0 - 1之间 float类型
+            temp_data[i, :, :, :] = np.asarray(img, dtype="float32").reshape(w, h, 1)
+            temp_label[i, :] = one_hot_labels
+            # print(i)
+            i += 1   
+            if i == batchsize:
+                dataset.resize([times*batchsize + batchsize, w, h, 1])
+                dataset[times*batchsize:times*batchsize+batchsize, :, :, :] = temp_data
+                labels.resize([times*batchsize + batchsize, class_num])
+                labels[times*batchsize:times*batchsize+batchsize, :] = temp_label
+                # print(labels.shape)
+                # print(dataset.shape)
+                times += 1
+                i = 0        
+    h5f.close()
+    print('h5py创建成功')     
+
+
+def read_h5py():
+    h5f = h5py.File('train.h5', 'r')
+    x = h5f['my_datas']
+    y = h5f['my_labels']
+    print('x.shape:  ' + str(x.shape))
+    print('y.shape:  ' + str(y.shape))
+    return x, y
 
 
 
-#create_csv()
+'''
+TFrecords
+'''
+def create_TFrecords(path, w, h,  classes_num):
+    num_classes = [i for i in range(classes_num)]
+    cwd = os.getcwd()
+    writer = tf.python_io.TFRecordWriter(path+".tfrecords")
+    for index, name in enumerate(num_classes):
+        class_path = cwd + "\\"+path+"\\" + str(name) + "\\"
+        for img_name in os.listdir(class_path):
+            img = Image.open(class_path + img_name).convert('L')  # 转化为灰度
+            img = img.resize((w, h))
+            img_raw = img.tobytes()  # 将图片转化为原生bytes
+            example = tf.train.Example(features=tf.train.Features(feature={
+                "my_labels": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+                'my_datas': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+            }))
+            writer.write(example.SerializeToString())
+    writer.close()
+    print('TFrecord创建成功')
 
-# create_h5py_batch(2, 28, 28, 10)  
-#create_h5py(28, 28, 10)   
-# read_h5py()
+
+def get_batch_TFrecords(imgs, labels, n_classes, batch_size):
+    imgs_batch, labels_batch = tf.train.shuffle_batch([imgs, labels], batch_size=batch_size, capacity=2000, min_after_dequeue=1000)
+    labels_batch = tf.one_hot(labels_batch, n_classes)
+    labels_batch = tf.cast(labels_batch, dtype=tf.int64)
+    labels_batch = tf.reshape(labels_batch, [batch_size, n_classes])
+    return imgs_batch, labels_batch   
 
 
-##x, y = read_npy()
-##shuffle_npy(x, y)
+def read_TFrecords(filename):
+    reader = tf.TFRecordReader()
+    filename_queue = tf.train.string_input_producer([filename])
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'my_labels': tf.FixedLenFeature([], tf.int64),
+                                           'my_datas': tf.FixedLenFeature([], tf.string), })
+    img = tf.decode_raw(features['my_datas'], tf.uint8)
+    img = tf.reshape(img, [28, 28, 1])
+    img = 1 - tf.cast(img, tf.float32) / 255.0
+    label = tf.cast(features['my_labels'], tf.int32)
+    return img, label
--- AI/cnn_Keras.py
+++ AI/cnn_Keras.py
@@ -1,5 +1,4 @@
-﻿import numpy as np
-import keras
+﻿import keras
 from keras.utils.io_utils import HDF5Matrix
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -7,40 +6,21 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.models import load_model
 import cv2
+from DataSet import *
 
-#(X_train, y_train), (X_test, y_test) = cifar100.load_data(label_mode='fine')
+class_num = 10
+img_w = 25
+img_h = 25
+x_train, y_train =read_h5py()
 
 
-
-def load_data(path, start_ix, n_samples):
-    X = HDF5Matrix(path, 'my_datas', start_ix, start_ix + n_samples)
-    y = HDF5Matrix(path, 'my_labels', start_ix, start_ix + n_samples)
-    return (X,y)
-#x_train, y_train = load_data(r'dataset/train.h5', 0, 5)
-
-x_train, y_train = np.load('X.npy'), np.load('Y.npy')
-x_train = x_train.reshape(-1, 50, 50, 1)
-
-print('shuffle前:  ' + str(y_train[0]))
-index=np.arange(len(y_train))
-np.random.shuffle(index)
-x_train=x_train[index] #X_train是训练集，y_train是训练标签
-y_train=y_train[index]
-print('shuffle后:  ' + str(y_train[0]))
-
-# x_train = HDF5Matrix(r'train.h5', 'my_datas')
-# y_train = HDF5Matrix(r'train.h5', 'my_labels')
-class_num=81
-
-# print(y_train.shape)
-# print(x_train.shape)
-# print(y_train[20008])
-# cv2.imshow('ss',x_train[20008] )
-# cv2.waitKey(0)
+print(y_train[0])
+cv2.imshow('ss',x_train[0] )
+cv2.waitKey(0)
 
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), strides=1, activation='relu', input_shape=(50, 50, 1)))
+model.add(Conv2D(32, (3, 3), strides=1, activation='relu', input_shape=(img_w, img_h, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
@@ -54,10 +34,8 @@ model.add(Dropout(0.5))
 model.add(Dense(class_num, activation='softmax'))
 
 Adam=keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
 model.compile(loss='categorical_crossentropy', optimizer=Adam ,metrics=['accuracy'])
-
-model.fit(x_train, y_train, batch_size=32, epochs=3, verbose=1, validation_split=0.5, shuffle="batch")
+model.fit(x_train, y_train, batch_size=64, epochs=1000, verbose=1, validation_split=0.1, shuffle="batch")
 model.save('my_model.h5')
 
 # score = model.evaluate(x_test, y_test, batch_size=32)
